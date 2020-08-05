531e6e228a2ca640bfd3c3a09058eea07ef1800a
Lunahri <jeremy17games@hotmail.com>
2020-08-05 18:25:09 -0300

    Add variable stop en funciones, que detiene el do.

--- 2.Problemas/WarCoin/WarCoin.cpp
+++ 2.Problemas/WarCoin/WarCoin.cpp
@@ -6,8 +6,8 @@
 
 //Protipos de funciones:
 void end();
-void CompuRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas);
-void UsuarioRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas);
+void CompuRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas,bool &stop);
+void UsuarioRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas,bool &stop);
 
 //Variables globales:
 
@@ -15,6 +15,7 @@ void UsuarioRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas);
 main()
 {
 	int CantidadMinima=0,CantidadMaxima=0,PilaMonedas=0;
+	bool stop=false;
 
 	printf("\nWARCOIN");
 	printf("\n=======");
@@ -45,41 +46,49 @@ main()
 	
 	do
 	{
-		UsuarioRetira(CantidadMinima,CantidadMaxima,PilaMonedas);
-		CompuRetira(CantidadMinima,CantidadMaxima,PilaMonedas);
+		UsuarioRetira(CantidadMinima,CantidadMaxima,PilaMonedas,stop);
+		CompuRetira(CantidadMinima,CantidadMaxima,PilaMonedas,stop);
 
-	} while (PilaMonedas!=0 and PilaMonedas>=CantidadMinima);
+	} while (stop==false);
 	
-	
-	
-	
-	
+
 	end();
 }
 
-void UsuarioRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas)
+void UsuarioRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas,bool &stop)
 {
 	int NumUsuario=0;
 
-	do
+	if (PilaMonedas!=0 and PilaMonedas>=CantidadMinima)
 	{
-		printf("\nIngresa la cantidad de monedas a eliminar: ");
-		scanf("%d",&NumUsuario);
-
-		if (NumUsuario>CantidadMaxima or NumUsuario<CantidadMinima)
+		do
 		{
-			printf("\nERROR: Se deben desapilar entre %d y %d monedas. Volver a intentar!!!!",CantidadMinima,CantidadMaxima);
-		}
+			printf("\nIngresa la cantidad de monedas a eliminar: ");
+			scanf("%d",&NumUsuario);
+
+			if (NumUsuario>CantidadMaxima or NumUsuario<CantidadMinima)
+			{
+				printf("\nERROR: Se deben desapilar entre %d y %d monedas. Volver a intentar!!!!",CantidadMinima,CantidadMaxima);
+			}
+			
+		} while (NumUsuario<CantidadMinima and NumUsuario<CantidadMaxima);
 		
-	} while (NumUsuario<CantidadMinima and NumUsuario<CantidadMaxima);
+		PilaMonedas-=NumUsuario;
+		EstadoPila(PilaMonedas);
+	}
+	else
+	{
+		printf("\nLA COMPUTADORA GANO!!! Puntaje obtenido: 0");
+		
+		stop=true;
+	}
 	
-	PilaMonedas-=NumUsuario;
-	EstadoPila(PilaMonedas);
+	
 }
 
 
 
-void CompuRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas)
+void CompuRetira(int CantidadMinima,int CantidadMaxima,int &PilaMonedas,bool &stop)
 {
 	int NumCompu=0;
 
