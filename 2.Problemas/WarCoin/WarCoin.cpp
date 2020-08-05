78eb7b2ad7000c91d2284e63bfc9b5121ee3c625
Lunahri <jeremy17games@hotmail.com>
2020-08-05 16:49:03 -0300

    Ingreso de dato y pila random.

--- 2.Problemas/WarCoin/WarCoin.cpp
+++ 2.Problemas/WarCoin/WarCoin.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<ctime>
 
 //Protipos de funciones:
 void end();
@@ -11,7 +12,20 @@ void end();
 
 main()
 {
+	int CantidadMinima=0,CantidadMaxima=0,PilaMondeas=0;
+
+	printf("\nWARCOIN");
+	printf("\n=======");
 	
+	printf("\nIngresa la cantidad minima de monedas que se pueden retirar: ");
+	scanf("%d",&CantidadMinima);
+
+	printf("\nIngresa la cantidad maxima de monedas que se pueden retirar: ");
+	scanf("%d",&CantidadMaxima);
+
+	srand(time(0));
+	PilaMonedas=10+rand()%50;
+	printf("\nLa cantidad inicial de monedas en la pila es: %d\n",PilaMonedas);
 	
 	end();
 }
