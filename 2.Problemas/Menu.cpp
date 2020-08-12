e0642d222bf592c4a168d9a3fc514671c63e1fe9
Lunahri <jeremy17games@hotmail.com>
2020-08-12 20:54:24 -0300

    Fase 1 menu xd.

--- 2.Problemas/Menu.cpp
+++ 2.Problemas/Menu.cpp
@@ -11,7 +11,36 @@ void end();
 
 main()
 {
-	
+	int opcion;
+
+
+    do
+    {
+        system("cls");
+        printf("\n\tBienvenidos a los juegos de consola!!");
+        printf("\n\nCategoria: Juegos numericos.");
+        printf("\n\n1.MayMen.");
+        printf("\n\n2.Adivinum.");
+        printf("\n\n3.WarCoin.");
+        printf("\n\nCategoria: Juegos de letras.");
+        printf("\n\n4.Ahorcado.");
+        printf("\n\n5. Salir.");
+        printf("\n\nIngrese el numero del juego que dsesea abrir: ");
+        scanf("%d",&opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            
+            break;
+        
+        default:
+            break;
+        }
+
+
+    } while (opcion!=5);
+    
 	
 	end();
 }
